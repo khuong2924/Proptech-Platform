b2a4d29228aacfed72e3f7bf4c77cda65a9c4e97
khuong2924 <truongkhuong666@gmail.com>
2024-11-30 03:58:27 +0700

    add template

---
+++ src/main/java/khuong/com/lasttermjava/LastTermJavaApplication.java
@@ -0,0 +1,13 @@
+package khuong.com.lasttermjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastTermJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LastTermJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/khuong/com/lasttermjava/LastTermJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package khuong.com.lasttermjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastTermJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
